bc8c928f514f0bae7573a5e8a8bf42b0350dae60
David Hatch <david@osohq.com>
2020-11-11 12:01:07 -0500

    Update versions for all python code.

--- scripts/release_bump_versions.sh
+++ scripts/release_bump_versions.sh
@@ -2,7 +2,14 @@
 
 set -xeuo pipefail
 
+if [[ $# -ne 3 ]]; then
+    echo "usage: next_version next_flask_version next_django_version"
+    exit 1
+fi
+
 NEXT_VERSION=$1
+NEXT_VERSION_FLASK=$2
+NEXT_VERSION_DJANGO=$3
 
 SCRIPT=$(realpath "$0")
 BASE_DIR=$(dirname $(dirname "$SCRIPT"))
@@ -12,7 +19,8 @@ echo "Updating versions to $1"
 function _sed {
     COMMAND="$1"
     FILE="$2"
-    sed -E -i '' -e "$COMMAND ; t ; q1 ;" "$FILE"
+    # TODO make sed fail of the replacement fails.
+    sed -E -i '' -e "$COMMAND" "$FILE"
 }
 
 function update_python_version {
@@ -23,3 +31,5 @@ function update_python_version {
 }
 
 update_python_version "$NEXT_VERSION" "$BASE_DIR/languages/python/oso/oso/oso.py"
+update_python_version "$NEXT_VERSION_FLASK" "$BASE_DIR/languages/python/flask-oso/flask_oso/__init__.py"
+update_python_version "$NEXT_VERSION_DJANGO" "$BASE_DIR/languages/python/django-oso/django_oso/__init__.py"
